bd49de6e14cca1c7195a2bcea87e78770894d68c
Sergei Eremkin <ffr@list.ru>
2022-08-28 02:07:06 +0300

    Added a user identification window

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+from email import message
+from multiprocessing.dummy import Manager
 from turtle import title
 from flask import Flask, render_template # вытащил объект
+from auth import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'world world world hello'
 
 
-@app.route('/') # Создание главной страницы - декоратор через @
+
+@app.route('/', methods =['GET', 'POST']) # Создание главной страницы - декоратор через @
 def main():
-     return render_template('base.html', title = 'Главная страница')
+     form = LoginForm()
+     if form.validate_on_submit():
+          name = form.name.data
+          surname = form.surname.data
+          email = form.email.data
+          print(name,surname,email)
+          return render_template('authorisation.html', title = 'Авторизация', 
+                              message ='Вы авторизовались')
+     return render_template('authorisation.html', title = 'Авторизация', 
+                              form = form)
+@app.route('/base')
+def base():
+     return render_template('base.html', title ='Главная')     
 
 @app.route('/myplans') # Создание главной страницы - декоратор через @
 def plan():
